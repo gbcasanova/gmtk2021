b33ca1efd40fbbdc2ddaee23d0e0002ea65d3ea4
Gabriel Casanova <48224013+casanovagames@users.noreply.github.com>
2021-06-11 17:25:45 -0300

    Draw sprite centered on the small collision box.

--- scripts/player.lua
+++ scripts/player.lua
@@ -9,11 +9,11 @@ local Player = Object:extend()
 function Player:new(scr, x, y)
     self.scr = scr
     self.x, self.y = x, y
-    self.w, self.h = 30, 65
+    self.w, self.h = 22, 9
     self.name = "Player"
     
     self.flip = 1
-    self.flipSpd = 0.2
+    self.flipSpd = 0.4
     self.spd = 150
     self.moving = false
 
@@ -28,7 +28,7 @@ function Player:new(scr, x, y)
     self.anim["walking"] = anim8.newAnimation(g('1-3', 2), 0.1)
     self.anim.current = self.anim["idle"]
 
-    scr.world:add(self, 0, 0, 30, 65) -- Add to collision world.
+    scr.world:add(self, self.x, self.y, self.w, self.h) -- Add to collision world.
 end
 
 function Player:update(dt)
@@ -69,14 +69,14 @@ end
 function Player:draw()
     -- Draw debug outline.
     if (_G.gameDebug) then
-        love.graphics.rectangle("line", self.x, self.y, self.w, self.h, 0, 0)
+        love.graphics.rectangle("line", self.x, self.y, self.w, self.h)
     end
 
     -- Draw sprite.
     self.anim.current:draw(
         self.sprite, 
-        self.x + self.sprW/2, 
-        self.y + self.sprH/2, 
+        (self.x + self.sprW/2) - (self.sprW/2 - self.w/2), 
+        (self.y + self.sprH/2) - (self.sprH - self.h), 
         0, self.flip, 1, 
         self.sprW/2,
         self.sprH/2
