d41a5f819b68c0093fd47ecdecc4e610e42709dd
Gabriel Casanova <48224013+casanovagames@users.noreply.github.com>
2021-06-11 16:39:47 -0300

    Added player sprite, objects table and collision world

--- screens/level.lua
+++ screens/level.lua
@@ -1,8 +1,16 @@
+-- Libs.
+local bump = require("libs.bump")
+
+-- Scripts.
+local Player = require("scripts.player")
+----------------------------------------
+
 local function loadAssets()
     local assets = {}
 
     -- Sprites.
     assets.sprites = {}
+    assets.sprites["drkarlovisky"] = love.graphics.newImage("assets/sprites/drkarlovisky.png")
 
     -- Music.
     assets.music = {}
@@ -15,19 +23,28 @@ end
 
 local screen = {}
 
-function screen:Load(ScreenManager) -- pass a reference to the ScreenManager. Avoids circlular require()
+function screen:Load(ScreenManager)
     collectgarbage()  -- Unload assets.
     self.assets = loadAssets() -- Load assets.
 
-    
+    self.world = bump.newWorld()
+
+    self.objects = {}
+    self.objects.player = Player(self, 0, 0)
 end
 
 function screen:Update(dt)
-    --
+    -- Update objects.
+    for i, v in pairs(self.objects) do
+        v:update(dt)
+    end
 end
 
 function screen:Draw()
-    --
+    -- Draw objects.
+    for i, v in pairs(self.objects) do
+        v:draw()
+    end
 end
 
 function screen:MousePressed(x, y, button)
