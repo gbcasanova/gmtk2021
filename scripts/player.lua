6151830d4a5b9db36cad3cac10cdfe5e8c90d413
Gabriel Casanova <48224013+casanovagames@users.noreply.github.com>
2021-06-11 17:08:13 -0300

    Added player animation

--- scripts/player.lua
+++ scripts/player.lua
@@ -10,15 +10,17 @@ function Player:new(scr, x, y)
     self.scr = scr
     self.x, self.y = x, y
     self.w, self.h = 30, 65
-    self.spd = 150
     self.name = "Player"
     
     self.flip = 1
     self.flipSpd = 0.2
+    self.spd = 150
+    self.moving = false
 
     -- Create sprite.
     self.sprite = scr.assets.sprites["drkarlovisky"]
-    local g = anim8.newGrid(30, 65, self.sprite:getWidth(), self.sprite:getHeight())
+    self.sprW, self.sprH = 30, 65
+    local g = anim8.newGrid(self.sprW, self.sprH, self.sprite:getWidth(), self.sprite:getHeight())
 
     -- Create animations.
     self.anim = {}
@@ -34,23 +36,34 @@ function Player:update(dt)
     self.anim.current:update(dt)
 
     -- Movement.
+    self.moving = false
     if (love.keyboard.isDown("up")) then
+        self.moving = true
         self.y = self.y - self.spd * dt
     elseif (love.keyboard.isDown("down")) then
+        self.moving = true
         self.y = self.y + self.spd * dt
     end
     if (love.keyboard.isDown("left")) then
+        self.moving = true
         self.x = self.x - self.spd * dt
         flux.to(self, self.flipSpd, {flip = 1})
     elseif (love.keyboard.isDown("right")) then
+        self.moving = true
         self.x = self.x + self.spd * dt
         flux.to(self, self.flipSpd, {flip = -1})
     end
 
-
     -- Collision resolution.
     local actualX, actualY, cols, len = self.scr.world:move(self, self.x, self.y)
     self.x, self.y = actualX, actualY
+
+    -- Animation.
+    if (self.moving) then
+        self.anim.current = self.anim["walking"]
+    else
+        self.anim.current = self.anim["idle"]
+    end
 end
 
 function Player:draw()
@@ -62,11 +75,11 @@ function Player:draw()
     -- Draw sprite.
     self.anim.current:draw(
         self.sprite, 
-        self.x + self.w/2, 
-        self.y + self.h/2, 
+        self.x + self.sprW/2, 
+        self.y + self.sprH/2, 
         0, self.flip, 1, 
-        self.w/2,
-        self.h/2
+        self.sprW/2,
+        self.sprH/2
     )
 end
 
