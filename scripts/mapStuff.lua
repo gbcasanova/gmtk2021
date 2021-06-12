2b4a47048ad07a45c3cfb938e1d390699a33ed81
Gabriel Casanova <48224013+casanovagames@users.noreply.github.com>
2021-06-12 13:46:33 -0300

    Updated tileset and level objects

--- scripts/mapStuff.lua
+++ scripts/mapStuff.lua
@@ -27,14 +27,15 @@ function mapStuff.createObjects(screen, map, layer, objsTable)
     -- Iterate through every object.
     for i, v in pairs(layer.objects) do
         local obj = v.type -- Get object name.
+        local x = v.x - 1 -- Y origin.
         local y = v.y - 32 - 1 -- Y origin.
 
         -- Create objects.
-        if (obj == "player") then objsTable.player = Player(screen, v.x, y)
-        elseif (obj == "coin") then table.insert(objsTable, Coin(screen, v.x, y))
-        elseif (obj == "spike") then table.insert(objsTable, Spike(screen, v.x, y))
-        elseif (obj == "redDemony") then table.insert(objsTable, Demony(screen, v.x, y, true))
-        elseif (obj == "blueDemony") then table.insert(objsTable, Demony(screen, v.x, y, false)) 
+        if (obj == "player") then objsTable.player = Player(screen, x, y)
+        elseif (obj == "coin") then table.insert(objsTable, Coin(screen, x, y))
+        elseif (obj == "spike") then table.insert(objsTable, Spike(screen, x, y))
+        elseif (obj == "redDemony") then table.insert(objsTable, Demony(screen, x, y, true))
+        elseif (obj == "blueDemony") then table.insert(objsTable, Demony(screen, x, y, false)) 
         end
     end
 end
